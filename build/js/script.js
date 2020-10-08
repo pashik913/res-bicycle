ec0d285109418af508a77131c770f12ff3a56031
panica913@gmail.com <panica913@gmail.com>
2020-10-08 15:49:19 +0300

    правки по змечаниям наставника

--- build/js/script.js
+++ build/js/script.js
@@ -4,12 +4,14 @@ var headerToggle = document.querySelector('.header__toggle');
 
 pageHeader.classList.remove('header--nojs');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('header--closed')) {
-    pageHeader.classList.remove('header--closed');
-    pageHeader.classList.add('header--opened');
-  } else {
-    pageHeader.classList.add('header--closed');
-    pageHeader.classList.remove('header--opened');
-  }
-});
+if (document.contains(pageHeader) && document.contains(headerToggle)) {
+  headerToggle.addEventListener('click', function () {
+    if (pageHeader.classList.contains('header--closed')) {
+      pageHeader.classList.remove('header--closed');
+      pageHeader.classList.add('header--opened');
+    } else {
+      pageHeader.classList.add('header--closed');
+      pageHeader.classList.remove('header--opened');
+    }
+  });
+}
